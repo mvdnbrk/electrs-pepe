6d802cb37fa964723bf24297942f0af2bd12d08e
Mark van den Broek <mvdnbrk@protonmail.com>
2026-02-12 16:00:54 +0100

    cleanup: update scripts, CI, and package metadata for Pepecoin

--- scripts/local-electrum.bash
+++ scripts/local-electrum.bash
@@ -2,7 +2,7 @@
 set -eux
 
 ADDR=127.0.0.1  # localhost
-PORT=50001      # default mainnet Electrum RPC port
+PORT=50002      # default mainnet Electrum RPC port (Pepecoin)
 PROTOCOL=t      # TCP (no SSL)
 
 # Use only local Electrum server:

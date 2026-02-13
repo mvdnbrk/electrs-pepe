4556300d185f26c224df701b6e52a0719657ad82
Mark van den Broek <mvdnbrk@protonmail.com>
2026-02-13 11:36:04 +0100

    Fix CI test failures by updating expected version string

--- tests/electrum.rs
+++ tests/electrum.rs
@@ -155,13 +155,13 @@ fn test_electrum_raw() {
     let write = "{\"jsonrpc\": \"2.0\", \"method\": \"server.version\", \"id\": 0}";
 
     let s = write_and_read(&mut stream, write);
-    let expected = "{\"id\":0,\"jsonrpc\":\"2.0\",\"result\":[\"electrs-esplora 0.4.1\",\"1.4\"]}";
+    let expected = "{\"id\":0,\"jsonrpc\":\"2.0\",\"result\":[\"electrs-esplora 0.4.1-pepe\",\"1.4\"]}";
     assert_eq!(s, expected);
 
     let write = "[{\"jsonrpc\": \"2.0\", \"method\": \"server.version\", \"id\": 0}]";
     let s = write_and_read(&mut stream, write);
     let expected =
-        "[{\"id\":0,\"jsonrpc\":\"2.0\",\"result\":[\"electrs-esplora 0.4.1\",\"1.4\"]}]";
+        "[{\"id\":0,\"jsonrpc\":\"2.0\",\"result\":[\"electrs-esplora 0.4.1-pepe\",\"1.4\"]}]";
     assert_eq!(s, expected);
 }
 
